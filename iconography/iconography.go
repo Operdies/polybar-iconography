d07cc60f6d151d226cbf49774e01091053ab897b
alex <alexnlarsen@gmail.com>
2022-09-14 22:03:02 +0200

    Functional madness

--- iconography/iconography.go
+++ iconography/iconography.go
@@ -5,6 +5,31 @@ import (
 	"time"
 )
 
+type Col = func(a func() string, c string) string 
+
+type Colorizer struct {
+  Foreground Col 
+  Background Col 
+  Accent Col
+}
+
+func getColorizer() Colorizer {
+  var colorizer Colorizer 
+  foregrounds := 0
+  fg := func(a func() string, c string) string {
+    foregrounds += 1
+    str := a()
+    fmt := "{F" + c + "}" + str
+    foregrounds -= 1
+    if foregrounds == 0 {
+      fmt += "{F-}"
+    }
+    return fmt
+	}
+  colorizer.Foreground = fg
+	return colorizer
+}
+
 func main() {
 	events := []string{
 		"node_add",
@@ -18,7 +43,10 @@ func main() {
 	for {
 		evt := <-source
 		fmt.Println(evt)
-		wmState := GetWmState()
-		fmt.Println(wmState.Monitors[0].Desktops[0])
+		// wmState := GetWmState()
+		// for _, mon := range wmState.Monitors {
+		// 	for _, desk := range mon.Desktops {
+		// 	}
+		// }
 	}
 }

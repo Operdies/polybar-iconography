be349e74b04a88bc28ee5fbb4fc13ced9d86ac5b
Alexander Larsen <alexnlarsen@gmail.com>
2023-01-31 10:42:19 +0100

    sock

--- main.go
+++ main.go
@@ -2,10 +2,48 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
-	"github.com/operdies/polybar-iconography/bspc"
-	"github.com/operdies/polybar-iconography/iconography"
+	"github.com/operdies/polybar-iconography/pkg/bspc"
+	// "github.com/operdies/polybar-iconography/iconography"
 )
+
+const sockfile = "/tmp/iconography.sock"
+
+func socker(){
+  socket, err := net.Listen("unix", sockfile)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  c := make(chan os.Signal, 1)
+  signal.Notify(c, os.Interrupt, syscall.SIGINT, syscall.SIGKILL)
+  fmt.Printf("Setting handler\n")
+  go func() {
+    sig := <-c 
+    fmt.Printf("%v Interrupt received\n", sig)
+    os.Remove(sockfile)
+    os.Exit(1)
+  }()
+
+  for {
+    conn, err := socket.Accept()
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    go func(conn net.Conn) {
+      defer conn.Close()
+    }(conn)
+  }
+
+
+}
 
 func main() {
 	events := []string{
@@ -18,13 +56,16 @@ func main() {
 
 	source := bspc.Subscribe(events, 0)
 
+  go socker()
+
 	for {
-		_, ok := <-source
+		e, ok := <-source
 		if !ok {
 			break
 		}
-		wm := bspc.GetWmState()
-		drawing := iconography.Draw(wm)
-		fmt.Println(drawing)
+    fmt.Printf("%v\n", e)
+		// wm := bspc.GetWmState()
+		// drawing := iconography.Draw(wm)
+		// fmt.Println(drawing)
 	}
 }
